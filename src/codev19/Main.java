51307c1ad4cbda8d79054d07688e0e2377ef5634
kwon <wingseter@gmail.com>
2021-01-14 16:27:25 +0900

    DB 데이터 분석 완료

--- src/codev19/Main.java
+++ src/codev19/Main.java
@@ -3,7 +3,6 @@ package codev19;
 import codev19.database.myDB;
 import codev19.model.Analyze;
 import codev19.config.sqlConfig;
-import codev19.utils.CovidCounter;
 import codev19.utils.Popup;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -20,7 +19,7 @@ public class Main extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
-    private ObservableList<Analyze> AnalyzeData = FXCollections.observableArrayList();
+    private final ObservableList<Analyze> AnalyzeData = FXCollections.observableArrayList();
 
     // 데이터배이스
     private myDB db;
@@ -28,8 +27,7 @@ public class Main extends Application {
     // 팝업
     private Popup alert;
 
-    // 분석기
-    private CovidCounter counter;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -51,9 +49,6 @@ public class Main extends Application {
         // 팝업 생성기
         alert = new Popup();
 
-        // 분석기
-        counter = new CovidCounter();
-
         // db 생성
         db = new myDB();
         // db 설정 불러오기
@@ -64,6 +59,7 @@ public class Main extends Application {
             alert.show("ERROR", "db connection error","connectDB has an ERROR");
         }
 
+        // 데이터 불러오기
         loadData();
     }
 
@@ -75,18 +71,108 @@ public class Main extends Application {
             return;
         }
 
-        while(db.next()){
-            while(counter.inputData(db.getResult(1), db.getResult(2),  db.getResult(3), db.getResult(4)
-                    , db.getResult(5), Boolean.parseBoolean(db.getResult(6)) ? 1 : 0)){
-                // 분석 결과 추가
-               AnalyzeData.add(counter.outputAnalyze());
-            }
-            // 사람 하나 추가
-            AnalyzeData.add(counter.outputData());
-        }
+        // 전체 개수 카운트
+        int TotalCount = 0;
+        int CityCount = 0;
+        int StateCount = 0;
+        int StreetCount = 0;
+        int ClinicCount = 0;
 
+        // 양성 환자 개수 카운트
+        int VirusTotal = 0;
+        int VirusState = 0;
+        int VirusCity = 0;
+        int VirusStreet = 0;
+        int VirusClinic = 0;
+
+        String state;   // 주
+        String city;    // 도시
+        String street;  // 로
+        String clinic;  // 진료소
+        String people;  // 사람
+
+
+        Analyze PrevData = null;   // 이전 데이터
+        Analyze NewData;    // 새로운 데이터
+
+        int flag; // 데이터 변화 플레그
+
+        while(db.next()){
+            flag = 0;
+
+            state = db.getResult(1);    // 주
+            city = db.getResult(2);     // 도시
+            street = db.getResult(3);   // 로
+            clinic = db.getResult(4);   // 진료소
+            people = db.getResult(5);   // 사람
+            int result = Boolean.parseBoolean(db.getResult(6)) ? 1: 0;  // 양성 or 음성
+            // 새로운 데이터로 테이블 ROW 생성
+            NewData = new Analyze(state, city, street, clinic, people, 1, result);
+
+            // 첫번째 데이터일 경우
+            if(PrevData == null){
+                PrevData = NewData;
+            }
+
+            // 검사 결과가 양성일 경우
+            if(result == 1){
+                VirusState++;
+                VirusCity++;
+                VirusStreet++;
+                VirusClinic++;
+                VirusTotal++;
+            }
+
+            // State가 이전 데이터랑 다른 경우
+            if(!PrevData.getState().equals(NewData.getState())){
+                flag = 4;
+            }
+            // City가 이전 데이터랑 다른 경우
+            else if(!PrevData.getCity().equals(NewData.getCity())){
+                flag = 3;
+            }
+            // Street가 이전 데이터랑 다른 경우
+            else if(!PrevData.getStreet().equals(NewData.getStreet())){
+                flag = 2;
+            }
+            // Clinic가 이전 데이터랑 다른 경우
+            else if(!PrevData.getClinic().equals(NewData.getClinic())){
+                flag = 1;
+            }
+
+            if(flag > 0){
+                AnalyzeData.add(new Analyze(PrevData.getState(), PrevData.getCity(), PrevData.getStreet(), PrevData.getClinic(), "", ClinicCount, VirusClinic));
+                ClinicCount = 0;
+                VirusClinic = 0;
+            }
+            if(flag > 1){
+                AnalyzeData.add(new Analyze(PrevData.getState(), PrevData.getCity(), PrevData.getStreet(), "", "", StreetCount, VirusStreet));
+                StreetCount = 0;
+                VirusStreet = 0;
+            }
+            if(flag > 2){
+                AnalyzeData.add(new Analyze(PrevData.getState(), PrevData.getCity(), "", "", "", CityCount, VirusCity));
+                CityCount = 0;
+                VirusCity = 0;
+            }
+            if(flag > 3){
+                AnalyzeData.add(new Analyze(PrevData.getState(), "", "", "", "", StateCount, VirusState));
+                StateCount = 0;
+                VirusState = 0;
+            }
+
+            TotalCount++;
+            CityCount++;
+            StateCount++;
+            StreetCount++;
+            ClinicCount++;
+
+            // 사람 하나 추가
+            AnalyzeData.add(new Analyze(state, city, street, clinic, people, 1, result));
+            PrevData = NewData;
+        }
         // 전체 종합 결과 추가
-        AnalyzeData.add(counter.totalAnalyze());
+        AnalyzeData.add(new Analyze("전국", "", "", "", "", TotalCount, VirusTotal));
     }
 
 
